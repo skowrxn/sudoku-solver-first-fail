bd3f6fc8f14d74d95188dfb296fac2fdfc6c65f2
Bartłomiej <bartomiejskowron@icloud.com>
2025-05-28 00:12:24 +0200

    Fix run_algorithm method

--- src/solvers/dancing_links_solver.py
+++ src/solvers/dancing_links_solver.py
@@ -15,37 +15,19 @@ class DancingLinksSudokuSolver(SudokuSolver):
     """
 
     def run_algorithm(self) -> SudokuGrid | None:
-        # TODO:
-        # This method will run the C program in a separate process.
-        # The new process will communicate with as via a queue.
-        # The basic example is shown in documentation:
-        # - https://docs.python.org/3/library/multiprocessing.html#exchanging-objects-between-processes
-        #
-        # 1. create a queue
-        # 2. create a process with:
-        # - target set to `self._communicate_with_external_solver`
-        # - args containing only the queue
-        # 3. start the process
-        # 4. get the result from the queue:
-        # - https://docs.python.org/3/library/queue.html#queue.Queue.get
-        # - set timeout to `self._time_limit`
-        # 5. the get method can raise an exception, if that happens
-        # - check if the process is still alive:
-        #   https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.is_alive
-        # - if it is, terminate it:
-        #   https://docs.python.org/3/library/multiprocessing.html#multiprocessing.Process.terminate
-        # - raise the TimeoutError
-
-        q = Queue()
-        p = Process(target=self._communicate_with_external_solver(q), args=(q,))
-        p.start()
-
+        queue = Queue()
+        process = Process(target=self._communicate_with_external_solver, args=(queue,))
+        process.start()
+        
         try:
-            return q.get(timeout=self._time_limit)
+            result = queue.get(timeout=self._time_limit)
+            return result
         except Exception:
-            if p.is_alive():
-                p.terminate()
+            if process.is_alive():
+                process.terminate()
             raise TimeoutError()
+
+        
 
     def _communicate_with_external_solver(self, queue: Queue) -> None:
         """
@@ -119,18 +101,11 @@ class DancingLinksSudokuSolver(SudokuSolver):
         solution_array: Array[c_int]
             a flat array of the same same as puzzle_array, but containing only zeros
         """
-
-       # TODO:
-        # Implement the method:
-        # - convert self._puzzle.size to `c_int`
-        #   https://docs.python.org/3/library/ctypes.html#fundamental-data-types
-        # - flatten the puzzle grid
-        #   check the `flatten` method of the `SudokuGrid`
-        # - and convert it to a C Array
-        #   https://docs.python.org/3/library/ctypes.html#arrays
-        # - create also an array full of `0`s
-        # Return them
-        raise NotImplementedError("not implemented — remove this line")
+        size = c_int(self._puzzle.size)
+        flat_puzzle = self._puzzle.flatten()
+        puzzle_array = (c_int * len(flat_puzzle))(*flat_puzzle)
+        solution_array = (c_int * len(flat_puzzle))(*[0] * len(flat_puzzle))
+        return puzzle_array, size, solution_array
 
     def _grid_from_array(self, solution: Array[c_int]) -> SudokuGrid:
         """
@@ -146,15 +121,10 @@ class DancingLinksSudokuSolver(SudokuSolver):
         grid: SudokuGrid
             a sudoku grid corresponding to the array
         """
-
-        # TODO:
-        # Implement the method:
-        # - convert array to np.array
-        #   * np.array can be initialized with anything iterable :)
-        # - reshape the new array to correct shape!
-        #   * https://numpy.org/doc/stable/reference/generated/numpy.reshape.html
-        # Return a new SudokuGrid with the hiven array
-        raise NotImplementedError("not implemented — remove this line")
+        array = np.array(solution)
+        size = self._puzzle.size
+        reshaped = array.reshape((size, size))
+        return SudokuGrid(reshaped)
 
     def _run_algorithm(self) -> SudokuGrid | None:
         """
